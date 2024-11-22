8ed2d315e696887751c8f578bb4a0547170dcc33
Trần Thái Ân <noyeye21@gmail.com>
2024-11-22 14:32:37 +0700

    bai hoc thu 16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,14 @@
+import React from "react";
+class MyComponents extends React.Component{ 
+    render(){
+        return(
+            <div> my first component
+            {Math.random()}
+
+            </div>
+        );
+    }
+
+}
+
+export default MyComponents;
