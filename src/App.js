1e67c683cd8fc38660b4a2ccbd1c91dff7ef02ee
Trần Thái Ân <noyeye21@gmail.com>
2024-11-29 14:24:42 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent'; 
+import './App.scss';
+
 
 const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
 
   return (
-    <div>
-      Trần Thái Ân &amp; Bin
-      <MyComponent>
-
-        
-      </MyComponent>
+    <div className="App-container">
+        hello world
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Trần Thái Ân
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
   );
 }
 
