893b765e91cceba86fa88ba3d48723c6b662c3d4
Trần Thái Ân <noyeye21@gmail.com>
2024-12-10 15:35:45 +0700

    chapter 6 1:38:04

--- src/components/content/manageuser.js
+++ src/components/content/manageuser.js
@@ -1,20 +1,27 @@
 import ModalCreateUser from "./ModalCreateUser";
+import './ManageUser.scss';
+import { FcPlus } from "react-icons/fc";
+import { useState } from "react";
 
 const ManageUser=(prosp)=>{
+    const [showModalCreatUser, setShowModalCreatUser]=useState(false);
     return (
         <div className="manage-user-container">
             <div className="title">
                 Manage User
             </div>
             <div className="user-content">
-                <button>
-                    add user
-                </button>
+                <div className="btn-add-new">
+                    <button className="btn btn-primary" onClick={()=>setShowModalCreatUser(true)}>
+                        <FcPlus/> add user
+                    </button>
+                </div>
+                <div className="table-users-container">
+                  table user 
+                </div>
+                <ModalCreateUser show={showModalCreatUser} setShow={setShowModalCreatUser}/>
             </div>
-            <div>
-                table user
-              <ModalCreateUser/>
-            </div>
+            
         </div>
     )
 }
