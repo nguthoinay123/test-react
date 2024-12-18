42e6da751b42fafa41d65c660f7335ae0d2854a6
Trần Thái Ân <noyeye21@gmail.com>
2024-12-18 14:14:13 +0700

    fix register

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -37,7 +37,7 @@ const postLogin=(email, password)=>{
     return axios.post(`api/v1/login`,{email, password});
 
 }
-const postSignup=(email, password)=>{
-    return axios.post(`api/v1/register`,{email, password});
+const postSignup=(email, password, username)=>{
+    return axios.post(`api/v1/register`,{email, password,username});
 }
 export {postCreateNewUser,getAllUser,putUpdateUser,deleteUser,getUserWithPaginate,postLogin, postSignup}
