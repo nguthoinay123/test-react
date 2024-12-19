0dd66b8664c73cb303b7fcd46faa21cef5a864a8
Trần Thái Ân <noyeye21@gmail.com>
2024-12-19 15:25:55 +0700

    redux

--- src/App.js
+++ src/App.js
@@ -6,27 +6,28 @@ import MyComponent from './components/MyComponent';
 
 const App = () => {
   const count = useSelector(state => state.counter.count);
+  // stateRedux => call Reducer => state's Reducer
   const dispatch = useDispatch();
 
   return (
-    <div>
-      Trần Thái Ân &amp; Bin
-      <MyComponent>
+    // <div>
+    //   Trần Thái Ân &amp; Bin
+    //   <MyComponent>
 
         
-      </MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Trần Thái Ân
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
+    //   </MyComponent>
     // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Trần Thái Ân
+        </p>
+        <div>Count = {count}</div>
+        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+      </header>
+    </div>
   );
 }
 
