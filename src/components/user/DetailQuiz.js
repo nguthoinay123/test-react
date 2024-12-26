8f777c665ea16655a47b756b0ec14b1e4498e47c
Trần Thái Ân <noyeye21@gmail.com>
2024-12-26 16:37:54 +0700

    lay data quiz

--- src/components/user/DetailQuiz.js
+++ src/components/user/DetailQuiz.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import { useEffect } from "react";
-
+import _ from "lodash"
 const DetailQuiz=(props)=>{
     const params = useParams();
     const quizId = params.id;
@@ -11,7 +11,27 @@ const DetailQuiz=(props)=>{
     },[quizId])
     const fetchQuestions = async() =>{
         let res = await getDataQuiz(quizId);
-        console.log('check',res)
+        console.log(res)
+        if(res&&res.EC===0){
+            let raw = res.DT
+            let data=_.chain(raw)
+            // Group the elements of Array based on `color` property
+            .groupBy("id")
+            // `key` is group's name (color), `value` is the array of objects
+            .map((value, key) => {  
+                let answers=[]
+                let questionDescripton, image=null
+                value.forEach((item, index)=>{
+                    if(index===0){
+                        questionDescripton=item.description;
+                        image=item.image
+                    }
+                    answers.push(item.answers);
+                })
+                return{ questionId: key, answers, questionDescripton, image }})
+            .value()
+            console.log('data',data)
+        }
     }
     return (
         <div className="detail-quiz-container">
